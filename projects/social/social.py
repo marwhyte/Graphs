1a517183d9c5afa65dc20e8ecc6d9cc7b68368cc
Marco Whyte <mwhyte@umass.edu>
2020-07-15 15:03:20 -0500

    mvp for social

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,27 @@
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
 
 class SocialGraph:
     def __init__(self):
@@ -44,9 +65,27 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        for user in range(num_users):
+            self.add_user(user)
 
-        # Create friendships
+        friendships = []
+        for user in range(1, self.last_id + 1):
+            for friend in range(user + 1, num_users + 1):
+                friendship = (user, friend)
+                friendships.append(friendship)
+
+        self.fisher_yates_shuffle(friendships)
+        total_friendships = num_users * avg_friendships
+
+        random_friendships = friendships[:total_friendships // 2]
+
+        for friendship in random_friendships:
+            self.add_friendship(friendship[0], friendship[1])
+
+    def fisher_yates_shuffle(self, l):
+        for i in range(0, len(l)):
+            random_index = random.randint(i, len(l) - 1)
+            l[random_index], l[i] = l[i], l[random_index]
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +98,23 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        q = Queue()
+
+        q.enqueue([user_id])
+
+        while q.size() > 0:
+            path = q.dequeue()
+
+            vertex = path[-1]
+
+            if vertex not in visited:
+                visited[vertex] = path
+                friends = self.friendships[vertex]
+                for friend in friends:
+                    path_copy = list(path)
+                    path_copy.append(friend)
+                    q.enqueue(path_copy)
+
         return visited
 
 
@@ -68,3 +124,6 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+
+
+# 10 users, 2 average friendships
